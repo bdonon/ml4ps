39c7fc01784a72ae5059d86314e26c3b113766d3
bdonon <balthazar.donon@gmail.com>
2022-09-12 17:58:32 +0200

    Implementation of torch Dataset, and in-depth cleaning.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,8 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
-    'sphinx.ext.intersphinx'
+    'sphinx.ext.intersphinx',
+    'nbsphinx'
 ]
 
 intersphinx_mapping = {
--- docs/source/usecase/powerflow.rst
+++
@@ -1,2 +0,0 @@
-Learning an AC Power Flow Approximation
-=======================================

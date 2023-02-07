36dd2fca4afa8d6400585f3c002a86fc7ef05c8f
bdonon <balthazar.donon@gmail.com>
2023-02-07 10:56:16 +0100

    neural_networks.get update

--- ml4ps/neural_network/__init__.py
+++ ml4ps/neural_network/__init__.py
@@ -1,4 +1,4 @@
-from ml4ps.neural_network.fully_connected import *
+from ml4ps.neural_network.fully_connected_old import *
 from ml4ps.neural_network.h2mgnode import *
 
 
@@ -6,6 +6,6 @@ def get(identifier, config):
     if identifier == 'fully_connected':
         return FullyConnected(**config)
     elif identifier == 'h2mgnode':
-        return H2MGNODE(**config)
+        return H2MGNODE.make(**config)
     else:
         raise ValueError('Neural network identifier {} not valid.'.format(identifier))

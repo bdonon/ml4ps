b78901dd69b36c38cebc04802636c78b373011d4
francoisCub <francois.cubelier@gmx.com>
2023-02-15 14:07:10 +0100

    Update Voltage management V2

--- ml4ps/environment/library/voltage_management/pandapower/VoltageManagementPandapowerV2.py
+++ ml4ps/environment/library/voltage_management/pandapower/VoltageManagementPandapowerV2.py
@@ -8,11 +8,11 @@ from .VoltageManagementPandapower import VoltageManagementPandapower
 
 from ml4ps import h2mg
 
+# TODO: How to use delta and instantaneous action forlulation with the same environment.
 class VoltageManagementPandapowerV2(VoltageManagementPandapower):
     """Power system environment for voltage management problem implemented
-        with pandapower that controls generators setpoints and shunt resources.
+        with pandapower that controls shunt resources.
 
-        The setpoints of the action directly override the current setpoints.
         For the shunt steps, the action encodes a variation of step 
             {0: -1 step, 1: no change, 2: +1 step}. The variations are applied and
             the result is clipped in range (0, max_step-1)
@@ -41,34 +41,22 @@ class VoltageManagementPandapowerV2(VoltageManagementPandapower):
 
     def __init__(self, data_dir, n_obj=None, max_steps=None, cost_hparams=None):
         super().__init__(data_dir, n_obj=n_obj, max_steps=max_steps, cost_hparams=cost_hparams)
-        self.ctrl_var_names = {"gen": ["vm_pu"],
-                               "ext_grid": ["vm_pu"],
-                               "shunt": ["step"]}
-        self.vlow = 0.8
-        self.vhigh = 1.2
-        self.action_space = spaces.Dict(
-            {"gen": spaces.Dict(
-                {"vm_pu": spaces.Box(low=self.vlow, high=self.vhigh, shape=(self.n_obj["gen"],))}),
-             "ext_grid": spaces.Dict(
-                {"vm_pu": spaces.Box(low=self.vlow, high=self.vhigh, shape=(self.n_obj["ext_grid"],))}),
-             "shunt": spaces.Dict(
-                {"delta_step": spaces.MultiDiscrete(nvec=np.full(shape=(self.n_obj["shunt"],), fill_value=3))}),
-             "stop": spaces.Discrete(2)})
+        self.ctrl_var_names = {"shunt": ["step"]}
+        self.action_space = spaces.Dict({"local_features": spaces.Dict(
+            {"shunt": spaces.Dict(
+                {"delta_step": spaces.MultiDiscrete(nvec=np.full(shape=(self.n_obj["shunt"],), fill_value=3))})
+             }),
+             "global_features": spaces.Dict({"stop": spaces.MultiBinary(1)})})
 
     def initialize_control_variables(self) -> Dict:
         """Inits control variable with default heuristics."""
         n_obj = self.n_obj
-        return {"gen": {"vm_pu": np.ones(shape=(n_obj["gen"],), dtype=np.float64)},
-                "ext_grid": {"vm_pu": np.ones(shape=(n_obj["ext_grid"],), dtype=np.float64)},
-                "shunt": {"step": np.zeros(shape=(n_obj["shunt"],), dtype=np.int32)}}
+        return {"local_features":{"shunt": {"step": np.zeros(shape=(n_obj["shunt"],), dtype=np.int32)}}}
 
     def update_ctrl_var(self, ctrl_var: Dict, action: Dict, state: VoltageManagementState) -> Dict:
         """Updates control variables with action."""
-        ctrl_var["gen"] = action["gen"]
-        ctrl_var["ext_grid"] = action["ext_grid"]
         max_step = self.backend.get_data_power_grid(
-            state.power_grid, feature_names={"shunt": ["max_step"]})
-        max_step = h2mg.local_features(max_step)
-        ctrl_var["shunt"]["step"] = np.clip((ctrl_var["shunt"]["step"] + action["shunt"]["delta_step"] - 1),
-                                            0, max_step["shunt"]["max_step"])
+            state.power_grid, local_feature_names={"shunt": ["max_step"]})
+        ctrl_var["local_features"]["shunt"]["step"] = np.clip((ctrl_var["local_features"]["shunt"]["step"] + action["local_features"]["shunt"]["delta_step"] - 1),
+                                            0, max_step["local_features"]["shunt"]["max_step"]-1)
         return ctrl_var

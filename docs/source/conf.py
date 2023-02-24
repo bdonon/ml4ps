5721159490771911c08c7e80b9fd9573913dbbbc
bdonon <balthazar.donon@gmail.com>
2023-02-24 16:25:32 +0100

    ml4ps logo.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,11 @@ templates_path = ['_templates']
 
 #html_theme = 'sphinx_rtd_theme'
 html_theme = "furo"
+html_favicon = "figure/ml4ps_logo_dark.png"
+html_theme_options = {
+    "light_logo": "figure/ml4ps_logo_light.png",
+    "dark_logo": "figure/ml4ps_logo_dark.png"
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'

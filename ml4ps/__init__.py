4bc33495e78bbc0121adb90c6f4a1225ba5c4859
bdonon <balthazar.donon@gmail.com>
2023-03-20 13:29:51 +0100

    ml4ps init compatible with major refactoring.

--- ml4ps/__init__.py
+++ ml4ps/__init__.py
@@ -8,11 +8,7 @@ __version__ = '0.0.1'
 from ml4ps.backend import *
 from ml4ps.neural_network import *
 from ml4ps.dataset import *
-from ml4ps.normalization import *
-from ml4ps.postprocessing import *
-from ml4ps.utils import *
 from ml4ps.pickle import *
-from ml4ps.transform import *
 from ml4ps import h2mg
 from ml4ps import reinforcement
 from ml4ps import logger

435ed763913730743b4d1264c96fd86b676be1e9
francoisCub <francois.cubelier@gmx.com>
2023-03-21 16:47:50 +0100

    Add test_policy

--- ml4ps/reinforcement/__init__.py
+++ ml4ps/reinforcement/__init__.py
@@ -1,3 +1,4 @@
 from ml4ps.reinforcement import environment
 from ml4ps.reinforcement import policy
 from ml4ps.reinforcement import algorithm
+from ml4ps.reinforcement.test_policy import test_policy
---
+++ ml4ps/reinforcement/test_policy.py
@@ -0,0 +1,16 @@
+from typing import Dict
+from ml4ps.reinforcement.environment import TestEnv, PSBaseEnv
+from ml4ps.reinforcement.policy import BasePolicy
+import tqdm
+
+def test_policy(single_env: PSBaseEnv, policy: BasePolicy, params: Dict, seed, output_dir: str):
+    test_env = TestEnv(single_env, save_folder=output_dir)
+    obs, info = test_env.reset()
+    with tqdm.tqdm(total=test_env.maxlen) as pbar:
+        while True:
+            action = policy.sample(params, obs, seed=seed, deterministic=True)
+            observation, reward, terminated, truncated, info = test_env.step(action)
+            if terminated:
+                pbar.update(1)
+            if test_env.is_done:
+                break

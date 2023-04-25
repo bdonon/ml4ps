c5a4cafa10ef012bdc87fea948d84b91ea77ec4c
francoisCub <francois.cubelier@gmx.com>
2023-04-25 11:03:50 +0200

    Update get_algorithm

--- ml4ps/reinforcement/algorithm/__init__.py
+++ ml4ps/reinforcement/algorithm/__init__.py
@@ -1,4 +1,11 @@
 from ml4ps.reinforcement.algorithm.reinforce import Reinforce
+from ml4ps.reinforcement.algorithm.reinforce_nn_baseline import ReinforceBaseline
 from ml4ps.reinforcement.algorithm.algorithm import Algorithm
-# from ml4ps.reinforcement.algorithm.reinforce_baseline import ReinforceWithBaseline
-# from ml4ps.reinforcement.algorithm.buffer import ReplayBuffer
+
+def get_algorithm(algorithm_type, *args, **kwargs) -> Algorithm:
+    if algorithm_type =="reinforce":
+        return Reinforce(*args, **kwargs)
+    elif algorithm_type== "reinforce_baseline":
+        return ReinforceBaseline(*args, **kwargs)
+    else:
+        raise ValueError("Algorithm not implemented: {algorithm_type}")

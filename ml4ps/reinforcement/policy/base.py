59766bdfa9d340193fdc2ef718d85e462b4ec366
francoisCub <francois.cubelier@gmx.com>
2023-06-26 11:28:23 +0200

    Update Basepolicy

--- ml4ps/reinforcement/policy/base.py
+++ ml4ps/reinforcement/policy/base.py
@@ -1,29 +1,38 @@
-from abc import ABC
-from typing import Tuple, Dict
 import os
+from abc import ABC
+from functools import partial
+from typing import Any, Dict, Tuple
 
-from ml4ps.h2mg import H2MG, H2MGNormalizer
 from gymnasium.vector import VectorEnv
+from jax import jit, vmap
+from ml4ps.h2mg import H2MG, H2MGNormalizer
 
 
 class BasePolicy(ABC):
+    normalizer: H2MGNormalizer = None
+
     def __init__(self) -> None:
         pass
 
     def init(self, rng, observation):
         pass
 
-    def log_prob(self, params, observation, action):
+    def log_prob(self, params: Dict, observation: H2MG, action: H2MG) -> Tuple[float, Any]:
         # return log probability of actions
         pass
 
-    def sample(self, params: dict, observation: dict, rng, deterministic: bool=False, n_action: int=1) -> Tuple[H2MG, float, Dict]:
+    def sample(self, params: dict, observation: dict, rng, deterministic: bool = False, n_action: int = 1) -> Tuple[H2MG, float, Dict]:
         # return both sample action and corresponding log probabilities
         pass
 
-    def vmap_sample(self, params: dict, observation: dict, rng, deterministic: bool=False, n_action: int=1) -> Tuple[H2MG, float, Dict]:
+    # @partial(jit, static_argnums=(0, 4, 5))
+    def vmap_sample(self, params: dict, observation: H2MG, rng, deterministic: bool = False, n_action: int = 1) -> Tuple[H2MG, float, Dict]:
         # return both sample action and corresponding log probabilities for a given batch
         pass
+
+    # @partial(jit, static_argnums=(0,))
+    def vmap_log_prob(self, params: Dict, obs: H2MG, action: H2MG) -> float:
+        return vmap(self.log_prob, in_axes=(None, 0, 0), out_axes=(0, 0))(params, obs, action)
 
     def _build_normalizer(self, env, normalizer_args=None):
         if isinstance(env, VectorEnv):
@@ -38,14 +47,16 @@ class BasePolicy(ABC):
             env_name = env.name
 
         normalizer_dir = os.path.join(data_dir, env_name)
-        normalize_path = os.path.join(normalizer_dir ,'normalizer.pkl')
+        normalize_path = os.path.join(normalizer_dir, 'normalizer.pkl')
         if os.path.exists(normalize_path):
             return H2MGNormalizer(filename=normalize_path)
         if normalizer_args is None:
-            normalizer = H2MGNormalizer(backend=backend, structure=observation_structure, data_dir=data_dir)
+            normalizer = H2MGNormalizer(
+                backend=backend, structure=observation_structure, data_dir=data_dir)
         else:
-            normalizer = H2MGNormalizer(backend=backend, structure=observation_structure, data_dir=data_dir, **normalizer_args)
-        
+            normalizer = H2MGNormalizer(
+                backend=backend, structure=observation_structure, data_dir=data_dir, **normalizer_args)
+
         if not os.path.exists(normalize_path):
             if not os.path.exists(normalizer_dir):
                 os.mkdir(normalizer_dir)

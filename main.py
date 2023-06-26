490c49d24ddfb47702122a9f41c972ff20f66c23
francoisCub <francois.cubelier@gmx.com>
2023-06-26 10:37:07 +0200

    Update main

--- main.py
+++ main.py
@@ -1,57 +1,88 @@
-import torch
-import ml4ps
-import gymnasium as gym
-import ml4ps
-import jax
-from hydra import compose, initialize
-from hydra.utils import instantiate
-import hydra
+import datetime
 import os
-os.environ["HYDRA_FULL_ERROR"]="1"
 
-def get_vector_env(*, env_name, num_envs, data_dir, **kwargs):
-    return gym.vector.make(env_name, data_dir=data_dir, num_envs=num_envs, soft_reset=False)
+import gymnasium as gym
+import hydra
+from gymnasium.vector import VectorEnv
+from omegaconf import OmegaConf
 
-def get_single_env(*, env_name, data_dir, **kwargs):
-    return gym.make(env_name, data_dir=data_dir)
+import ml4ps
+from ml4ps.logger import log_params_from_omegaconf_dict
+from ml4ps.reinforcement.algorithm import get_algorithm
 
-def get_config():
-    return {}
+os.environ["HYDRA_FULL_ERROR"] = "1"
 
-def get_logger(*, experiment_name, **kwargs):
-    return ml4ps.logger.MLFlowLogger(experiment_name=experiment_name)
+OmegaConf.register_new_resolver("add", lambda x, y: x+y)
+OmegaConf.register_new_resolver("mul", lambda x, y: x*y)
 
 
-def instantiate_algorithm(algo_cfg):
-    return instantiate(algo_cfg)
+def get_vector_env(*, name, **kwargs) -> VectorEnv:
+    return gym.vector.make(name, **kwargs)
 
-def get_algorithm(*, env, policy_type, **kwargs):
-    # ml4ps.reinforcement.algorithm.get(env, **kwargs)
-    return ml4ps.reinforcement.algorithm.Reinforce(env, policy_type=policy_type, **kwargs)
+
+def get_single_env(*, name, **kwargs):
+    return gym.make(name, **kwargs)
+
+
+def get_logger(*, experiment_name, run_name, **kwargs):
+    return ml4ps.logger.MLFlowLogger(experiment_name=experiment_name, run_name=run_name, **kwargs)
+
+
+def save_config(cfg):
+    if not os.path.isdir(cfg.res_dir):
+        os.mkdir(cfg.res_dir)
+    if cfg.run_name is None:
+        run_dir = os.path.join(
+            cfg.res_dir, f'algo_{datetime.datetime.now().strftime("%m%d%Y_%H%M%S")}')
+    else:
+        run_dir = os.path.join(cfg.res_dir, cfg.run_name)
+    if not os.path.isdir(run_dir):
+        os.mkdir(run_dir)
+    with open(os.path.join(run_dir, "config.yaml"), "w") as f:
+        OmegaConf.save(cfg, f)
+    return run_dir
+
+
+def init_envs(cfg):
+    env = get_vector_env(**cfg.env)
+    env.reset(seed=cfg.seed)
+    val_env = get_single_env(**cfg.val_env)
+    val_env.reset(seed=cfg.seed)
+    test_env = get_single_env(**cfg.test_env)
+    test_env.reset(seed=cfg.seed)
+    return env, val_env, test_env
+
 
 @hydra.main(version_base=None, config_path="config", config_name="config")
 def main(cfg):
+    # Save configuration
+    run_dir = save_config(cfg)
 
     # Training environment
-    env = get_vector_env(env_name=cfg.env.name, data_dir=cfg.env.data_dir, num_envs=cfg.env.num_envs, train=True)
-    val_env = get_vector_env(env_name=cfg.val_env.name, data_dir=cfg.val_env.data_dir, num_envs=cfg.val_env.num_envs, train=True)
-    test_env = get_single_env(env_name=cfg.test_env.name, data_dir=cfg.test_env.data_dir)
-
+    env, val_env, test_env = init_envs(cfg)
 
     # RL algorithm
-    algorithm = get_algorithm(env=env, val_env=val_env, test_env=test_env, **cfg.algorithm)
-
+    algorithm = get_algorithm(algorithm_type=cfg.algorithm_type, env=env,
+                              val_env=val_env, test_env=test_env, run_dir=run_dir, **cfg.algorithm)
 
     # Logger
     logger = get_logger(**cfg.logger)
-    
+    log_params_from_omegaconf_dict(cfg)
+
     # Learning loop
-    algorithm.learn(logger=logger, seed=cfg.seed, batch_size=cfg.batch_size, **cfg.learn)
+    algorithm.learn(logger=logger, seed=cfg.seed,
+                    batch_size=cfg.batch_size, **cfg.learn)
+    env.close()
+    val_env.close()
 
-    # Evaluation loop
-    # algorithm.eval(logger=logger, seed=cfg.seed, batch_size=cfg.batch_size, **cfg.learn)
+    # Save
+    algorithm.save(run_dir)
 
-    algorithm.test(test_env=test_env, res_dir=cfg.res_dir)
+    # Evaluation
+    algorithm.test(test_env=test_env, res_dir=run_dir, **cfg.test)
+
+    logger.finalize()
+
 
 if __name__ == "__main__":
     main()

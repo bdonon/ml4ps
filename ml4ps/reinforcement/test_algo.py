27bbf0ffef2de12d6c7f9441f61c294893a919f1
francoisCub <francois.cubelier@gmx.com>
2023-07-14 16:19:50 +0200

    Add more info to test_algo

--- ml4ps/reinforcement/test_algo.py
+++ ml4ps/reinforcement/test_algo.py
@@ -9,13 +9,16 @@ from omegaconf import OmegaConf
 import os
 import pickle
 import gymnasium as gym
+import json
 
 
 def test_algo(folder, save_folder: str, algorithm: Algorithm=None, test_env_name=None, test_env_dir=None, test_env_args=None):
         if algorithm is None:
             best_params_name = Algorithm.best_params_name
+            best_params_info_name = Algorithm.best_params_info_name
         else:
             best_params_name = algorithm.best_params_name
+            best_params_info_name = algorithm.best_params_info_name
         
         cfg = OmegaConf.load(os.path.join(folder, "config.yaml"))
         if test_env_name is None:
@@ -30,14 +33,21 @@ def test_algo(folder, save_folder: str, algorithm: Algorithm=None, test_env_name
         best_params_path = os.path.join(folder, best_params_name)
         with open(best_params_path, 'rb') as f:
             params = pickle.load(f)
+        with open(os.path.join(folder, best_params_info_name), 'r') as f:
+            params_info = json.load(f)
+            print("Best params info:", params_info)
         train_policy = get_policy(cfg.algorithm.policy_type, train_env, **cfg.algorithm.policy_args, nn_args=cfg.algorithm.nn_args)
         policy = get_policy(cfg.algorithm.policy_type, test_env, **cfg.algorithm.policy_args, nn_args=cfg.algorithm.nn_args, normalizer=train_policy.normalizer)
         train_env.close()
         save_path = os.path.join(folder, save_folder)
         if not os.path.exists(save_path):
             os.mkdir(save_path)
-        eval_reward(test_env, policy, params, seed=0, max_steps=cfg.max_steps, save_folder=save_path)
+        value, eval_info = eval_reward(test_env, policy, params, seed=0, max_steps=cfg.max_steps, save_folder=save_path)
         test_env.close()
+
+        with open(os.path.join(save_path, 'info.json'), 'w') as f:
+            json.dump({"training_step": params_info.get("step"), "validation_value": params_info.get("value"),
+                            "test_value": value, **eval_info}, f)
 
 
 def get_single_env(*, name, **kwargs):
